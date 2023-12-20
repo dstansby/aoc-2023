eee72c367d6e0e00b79cbb30d29e94215b3f9c29
David Stansby <dstansby@gmail.com>
2023-12-20 11:02:54 +0000

    Working 17

--- 17.py
+++ 17.py
@@ -8,8 +8,6 @@ from copy import copy
 
 from typing import Literal
 
-from functools import cache
-
 Step = Literal["U", "D", "L", "R"]
 
 
@@ -20,7 +18,7 @@ class Index(tuple[int, int]):
 
 @dataclass
 class StepChain:
-    idx: tuple[int, int]
+    idx: Index
     previous_steps: str
     total: int
 
@@ -31,10 +29,7 @@ steps = {
     "D": Index((1, 0))
 }
 
-def out_of_bounds(arr, idx) -> bool:
-    return any(i < 0 for i in idx) or idx[0] >= arr.shape[0] or idx[1] >= arr.shape[1]
-
-@profile
+# @profile
 def min_length(arr, min_steps: int, max_steps: int) -> StepChain:
     idx = Index((0, 0))
     chains: deque[StepChain] = deque()
@@ -42,6 +37,7 @@ def min_length(arr, min_steps: int, max_steps: int) -> StepChain:
     chains.append(StepChain(idx, "D", 0))
 
     best_chain = StepChain(idx, "", 1_000_000)
+    best_chains = []
 
     seen = dict()
 
@@ -49,57 +45,57 @@ def min_length(arr, min_steps: int, max_steps: int) -> StepChain:
     next_steps_dict = {
         "R": ["U", "D"],
         "L": ["U", "D"],
-        "U": ["R", "L"],
-        "D": ["R", "L"]
+        "U": ["L", "R"],
+        "D": ["L", "R"]
     }
     counter = 0
 
     while len(chains):
         chain = chains.popleft()
 
-        last_step = chain.previous_steps[-1]
-        next_steps = next_steps_dict[last_step]
-
-        for next_step in next_steps:
+        # Loop through L/R or U/D
+        for next_step in next_steps_dict[chain.previous_steps[-1]]:
             new_tot = chain.total
+            next_idx = copy(chain.idx)
             
             for n_steps in range(1, max_steps + 1):
-                next_idx = Index(
-                    (
-                        chain.idx[0] + steps[next_step][0] * n_steps,
-                        chain.idx[1] + steps[next_step][1] * n_steps,
-                     )
-                )
+                # Take one more step
+                next_idx += steps[next_step]
+            
+                if next_idx[0] < 0 or next_idx[1] < 0:
+                    # Further steps will be out of bounds
+                    break
+
+                # Update total
                 try:
                     new_tot += arr[next_idx[0], next_idx[1]]
                 except IndexError:
                     # Further steps will be out of bounds
                     break
+                
 
                 if n_steps < min_steps:
-                    continue
-
-                if next_idx[0] < 0 or next_idx[1] < 0:
-                    # Further steps will be out
+                    # Go to next number of steps,
+                    # having accumulated the total on this step
                     continue
                 
                 if next_idx == final_idx:
+                    # At the end
                     if new_tot < best_chain.total:
                         best_chain = StepChain(next_idx, chain.previous_steps + next_step * n_steps, new_tot)
+                        best_chains.append(best_chain)
                     # Rest will be out of bounds so just break
                     break
                 
                 # If we've taken this step before, and the cost was lower than our current cost
                 if seen.get((chain.idx, next_idx), 1_000_000) <= new_tot:
                     continue
-        
-                
+
                 chains.append(StepChain(next_idx, chain.previous_steps + next_step * n_steps, new_tot))
                 seen[(chain.idx, next_idx)] = new_tot
-                next_idx = next_idx + steps[next_step]
 
         counter += 1
-        if counter % 10000 == 0:
+        if counter % 2**12 == 0:
             print(len(chains))
 
     return best_chain
